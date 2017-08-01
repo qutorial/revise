f57a2bb88ae874aa4c4dd7bc1cd264239cbe9214
Zaur <zaur@genua.de>
2017-08-02 00:02:04 +0200

    customize

--- customize.sh
+++ customize.sh
@@ -11,22 +11,26 @@ files=( "package.json" )
 for f in "${files[@]}"
 do
   echo Preparing $f
+
   rand1=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
   rand2=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
   rand3=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
   rand4=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
   rand5=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
 
-  sed "s/SSsmallSS/$small/g" $f |\
+  sed "s/SSsmallSS/$small/g" "$f" |\
 	sed "s/SSbigSS/$big/g" |\
 	sed "s/SSnameSS/$name/g" |\
 	sed "s/SSrand1SS/$rand5/g" |\
 	sed "s/SSrand2SS/$rand4/g" |\
 	sed "s/SSrand3SS/$rand3/g" |\
 	sed "s/SSrand4SS/$rand2/g" |\
-	sed "s/SSrand5SS/$rand1/g" |\
-      > tmp/output.txt
-  mv tmp/output.txt $f
+	sed "s/SSrand5SS/$rand1/g" 2>&1 > ./tmp/output.txt
+
+  mv ./tmp/output.txt "$f"
+  cat "$f"
+
+
 done
 
 
