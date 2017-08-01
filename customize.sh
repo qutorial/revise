e3bc9867537c878426489fb710081f24e2ab99fe
Zaur <zaur@genua.de>
2017-08-02 00:25:59 +0200

    check added

--- customize.sh
+++ customize.sh
@@ -38,3 +38,11 @@ do
 done
 
 
+#how many were not changed
+
+rest=`grep -irn -e "SSsmallSS" -e "SSbigSS" -e "SSrand" -e "SSdomain" -e "SSname" -e "SSpassscore" | wc -l
+
+if [[ $rest -ne 0 ]]; then
+  echo "Haven't changed some files! Use grep to find them!"
+  echo "Error!!!"
+fi
