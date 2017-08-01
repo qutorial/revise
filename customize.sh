2f28e7a70ef446d573a650ca59ef2e9bbbc01c57
Zaur <zaur@genua.de>
2017-08-01 23:58:00 +0200

    rand feature

--- customize.sh
+++ customize.sh
@@ -6,15 +6,26 @@ read -p "Please, give your app a name, e.g. Apple Pie: " name
 big=`echo $name | sed 's/[[:blank:]]//g'`
 small=`echo "$big"  | tr '[:upper:]' '[:lower:]'`
 
-echo $big
-echo $small
-
 files=( "package.json" )
 
 for f in "${files[@]}"
 do
   echo Preparing $f
-  sed "s/SSsmallSS/$small/g" $f | sed "s/SSbigSS/$big/g" > tmp/output.txt
+  rand1=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
+  rand2=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
+  rand3=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
+  rand4=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
+  rand5=`hexdump -n 64 -e '4/4 "%08X" 1 ""' /dev/urandom`
+
+  sed "s/SSsmallSS/$small/g" $f |\
+	sed "s/SSbigSS/$big/g" |\
+	sed "s/SSnameSS/$name/g" |\
+	sed "s/SSrand1SS/$rand5/g" |\
+	sed "s/SSrand2SS/$rand4/g" |\
+	sed "s/SSrand3SS/$rand3/g" |\
+	sed "s/SSrand4SS/$rand2/g" |\
+	sed "s/SSrand5SS/$rand1/g" |\
+      > tmp/output.txt
   mv tmp/output.txt $f
 done
 
