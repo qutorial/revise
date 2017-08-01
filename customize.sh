5ddfa458a767e7739520f7f0a41e0ebf0ad4f1c8
Zaur <zaur@genua.de>
2017-08-01 23:49:26 +0200

    Preparing cusomize script

---
+++ customize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+read -p "Please, give your app a name, e.g. Apple Pie: " name
+
+big=`echo $name | sed 's/[[:blank:]]//g'`
+small=`echo "$big"  | tr '[:upper:]' '[:lower:]'`
+
+echo $big
+echo $small
+
+files=( "package.json" )
+
+for f in "${files[@]}"
+do
+  echo Preparing $f
+  sed "s/SSsmallSS/$small/g" $f | sed "s/SSbigSS/$big/g" > $f
+done
+
+
